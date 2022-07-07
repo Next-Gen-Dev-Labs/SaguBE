6ec6c1347a6b21fa59c0007a8f5fa71c19ef74ae
victor orlunda <vicorlunda@gmail.com>
2022-07-07 06:37:14 +0100

    add a nonce factor constant

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -2,6 +2,8 @@ export const META_SIGN_MESSAGE = 'I am signing my one time nonce: ';
 
 export const SALT = 10;
 
+export const NONCE_FACTOR = 100_000;
+
 export const JWT_EXPIRATION = '3h';
 
 export const JWT_REFRESH_EXPIRATION = '4320h';
