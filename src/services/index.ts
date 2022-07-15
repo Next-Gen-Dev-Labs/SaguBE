2fefa0cf61f724e34e832e21d05a9e977a4092fe
victor orlunda <vicorlunda@gmail.com>
2022-07-15 08:14:43 +0100

    mount misc router

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,6 +1,8 @@
 import { Router } from 'express';
 import { authRouter } from './auth';
+import { miscRouter } from './misc';
 
 export const services: Router = Router();
 
 services.use('/auth', authRouter);
+services.use('/misc', miscRouter);
