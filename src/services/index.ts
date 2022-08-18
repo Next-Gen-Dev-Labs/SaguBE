9ee4e1eb43a6e6ff1c88d0214d4552fa8ee243dd
victor orlunda <vicorlunda@gmail.com>
2022-08-18 21:46:42 +0100

    mount ticket router

--- src/services/index.ts
+++ src/services/index.ts
@@ -2,9 +2,11 @@ import { Router } from 'express';
 import { authRouter } from './auth';
 import { eventRouter } from './events';
 import { miscRouter } from './misc';
+import { ticketRouter } from './tickets';
 
 export const services: Router = Router();
 
 services.use('/auth', authRouter);
 services.use('/events', eventRouter);
 services.use('/misc', miscRouter);
+services.use('/ticket', ticketRouter);
