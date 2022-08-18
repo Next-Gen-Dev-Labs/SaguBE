70a7f249decaef98ce11636707b253a92992b4b9
victor orlunda <vicorlunda@gmail.com>
2022-08-18 21:45:23 +0100

    create middleware validator for creating tickets

---
+++ src/services/tickets/middlewares.ts
@@ -0,0 +1,19 @@
+import { Request, Response, NextFunction } from 'express';
+import schemas from './schemas';
+
+export default {
+  /**
+   *
+   * create Ticket middleware
+   *
+   */
+
+  async createTicket(req: Request, res: Response, next: NextFunction) {
+    try {
+      await schemas.createTicket.parseAsync(req.body);
+      next();
+    } catch (error) {
+      next(error);
+    }
+  },
+};
