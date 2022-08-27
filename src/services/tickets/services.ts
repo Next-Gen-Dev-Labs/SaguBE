2f16486dfaacb1523351b6441558d5eabf22d018
victor orlunda <vicorlunda@gmail.com>
2022-08-27 21:46:56 +0100

    create service to list all ticket minted

--- src/services/tickets/services.ts
+++ src/services/tickets/services.ts
@@ -6,7 +6,7 @@ import {
   IMintedTickets,
   mintedModel,
 } from '../../models';
-import { BaseError, postData } from '../../commons';
+import { BaseError, postData, refinePaginators } from '../../commons';
 import { PINATA_BASE_URL, pinata_jwt } from '../../config';
 import { Types } from 'mongoose';
 
@@ -146,6 +146,23 @@ export default {
       })
       .lean();
 
+    return { tickets };
+  },
+
+  /**
+   *
+   * list all minted tickets
+   *
+   */
+
+  async listMintedTickets(params: { skip?: string; limit?: string }) {
+    const { skip, limit } = params;
+    const paginators = refinePaginators({
+      skip: <string>skip,
+      limit: <string>limit,
+    });
+
+    const tickets = await mintedModel.find({}, {}, { ...paginators });
     return { tickets };
   },
 };
